51e48435b6393d50b363d02e89f9454cbd8b3643
evan <aspaasevan@gmail.com>
2020-03-25 17:35:13 -0700

    working with yup

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Yup from 'yup';
+import './Form.css';
+
+// validating name and email with Yup
+const formSchema = Yup.object().shape({
+  name: Yup
+    .string()
+    .required("Name is a required field."),
+  email: Yup
+    .string()
+    .email("Must be a valid email address.")
+    .required("Must include email address.")
+});
 
 function Form() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    password: ''
+    password: '',
+    terms: ''
   });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    terms: ''
+  });
+
+  const [post, setPost] = useState([]);
+
 
   const handleChange = (event) => {
     setFormData({
@@ -19,24 +42,44 @@ function Form() {
     setFormData({ 
       name: '',
       email: '',
-      password: ''
+      password: '',
+      terms: ''
     });
   };
   
   return(
-    <div className=''>
+    <div className='form-container'>
       <form onSubmit={handleSubmit}>
         <label htmlFor='name'>
           Name: 
-          <input type='text' name='name' value={formData.name} placeholder='full name' onChange={handleChange} />
+          <input 
+            type='text' 
+            name='name' 
+            value={formData.name} 
+            placeholder='full name' 
+            onChange={handleChange} 
+          />
         </label>
+        {/* {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null} */}
         <label htmlFor='email'>
           Email: 
-          <input type='text' name='email' value={formData.email} placeholder='email' onChange={handleChange} />
+          <input 
+            type='text' 
+            name='email' 
+            value={formData.email} 
+            placeholder='email' 
+            onChange={handleChange} 
+          />
         </label>
+        {/* TODO errors for email here */}
         <label htmlFor='password'>
           Password: 
-          <input type='text' name='password' value={formData.password} onChange={handleChange} />
+          <input 
+            type='text' 
+            name='password' 
+            value={formData.password} 
+            onChange={handleChange} 
+          />
         </label>
         <label htmlFor='terms'>
           Terms of Service:
@@ -47,7 +90,10 @@ function Form() {
             onChange={handleChange} 
           />
         </label>
-        <input type='submit' value='Submit' />
+        <input 
+          type='submit' 
+          value='Submit' 
+        />
       </form>
     </div>
   );
